4e650e14014449aba2719780a45b2184753c60f7
GiuliaCondorelli <giuliacondorelli99@gmail.com>
2025-07-08 10:59:15 +0200

    Update 08_spatial_variability.R

--- Code/08_spatial_variability.R
+++ Code/08_spatial_variability.R
@@ -1,4 +1,5 @@
-# Code for calculating spatial variability
+
+# Code fore calculating spatial variability
 
 library(terra)
 library(imageRy)
@@ -6,104 +7,117 @@ library(viridis)
 library(patchwork)
 library(RStoolbox)
 
-# Theory: 
+#Theory
+#Standard deviation
+23,22,23,49
 
-# Standard deviation
-23, 22, 23, 49
+m = (23+22+23+49) / 4  #media
+m=29.25
 
-m = (23 + 22 + 23 + 49) / 4
-# m = 29.25
+#variability
+num = (23-29.25)^2 + (22-29.25)^2 + (23-29.25)^2 + (49-29.25)^2 #scarto quadratico
+den = 4
+variance = num / den #scarto quadratico medio
+stedv = sqrt (variance) #deviazione standard
+stedv =11.41271
 
-num = (23-29.25)^2 + (22-29.25)^2 + (23-29.25)^2 + (49-29.25)^2     # estraiamo la somma di tutti gli scarti quadratici
-den=4
-
-variance = num/den  # è lo scarto quadratico medio
-stdev = sqrt(variance)
-# stdev = 11.41271
-
-sd(c(23, 22, 23))
-
-#---
+#or
+sd (c(23,22,23,49))
 
 im.list()
-
 sent = im.import("sentinel.png")
-sent = flip(sent)
+sent= flip(sent)
 
-# band 1 = NIR
-# band 2 = red
-# band 3 = green
-# Exercise plot the image in RGB with the NIR on top of the red component
+#band 1 = NIR
+#band 2 = red
+#band 3 = green
+
+#Exercise: plot the image in RGB whit the NIR ontop of the red component
 im.plotRGB(sent, r=1, g=2, b=3)
 
-# Exercise: make three plots with NIR on top of each component: r, g, b
+#Exercise: make three plots with NIR ontop of each component: r, g, b
 im.multiframe(1,3)
+
 im.plotRGB(sent, r=1, g=2, b=3)
 im.plotRGB(sent, r=2, g=1, b=3)
 im.plotRGB(sent, r=3, g=2, b=1)
 
-nir = sent[[1]]
+nir= sent[[1]]
+plot(nir)
 
-# Exercise: plot the nir band with the inferno color ramp palette
-plot(nit, col=inferno(100))
+Exercise: plot the NIR band with the inferno color ramp palette
+plot(nir, col=inferno(100))
 sd3 = focal(nir, w=c(3,3), fun=sd)
 plot(sd3)
 
 im.multiframe(1,2)
-im.plotRGB(sent,r=1,g=2,b=3)
+im.plotRGB(sent, r=1, g=2, b=3)
+sd3 = focal(nir, w=c(3,3), fun=sd)
 plot(sd3)
 
-# Exercise: calculate standard deviaton of the nir band with a moving window of 5x5 pixels
-sd5= focal(nir,w=c(5,5), fun=sd)
+#Exercise: calculates standard deviation of the nir band whit a moving window of 5x5 pixels
+sd5 = focal(nir, w=c(5,5), fun=sd)
+plot(sd5)
 
-# Exercise: use ggplot to plot the standard deviation
+#Exercise: use ggplot to plot the standard deviation
 im.ggplot(sd3)
 
-# Exercise: plot the two sd maps (3 and 5) one beside the other with ggplot
+#Exercise: plot the two sd maps (3 and 5) one beside the other whit ggplot
 p1 = im.ggplot(sd3)
-p2 = im.ggplot(sd5)
+p2 = im.ggplo(sd5)
 p1 + p2
 
-# Exercise: with ggplot, plot the original set in RGB (ggRGB) together with the sd with 3 and 5 pixels
-p3 = ggRGB(sent, r=1, g=2,  b=3)
-p1 + p2 + p3
+#Exercise: whit ggplot, plot the original set in RGB (ggRGB) togheter with the sd with 3 and 5 pixels
+p3= ggRGB(sent, r=1, g=2, b=3)
 p3 + p1 + p2
 
-# What to do in case of huge images
+#What to do in case of huge images
+sent = im.import("sentinel.png")
+sent= flip(sent)
+plot(sent)
+ncell(sent) 
 
 ncell(sent) * nlyr(sent)
+#794 * 798
+#2534448
 
-# 794 * 798
-# 2534448
-
-senta = aggregate(sent, fact=2) 
+senta = aggregate (sent, fact=2)
 ncell(senta) * nlyr(senta)
-# 633612
+#633612
 
-senta5 = aggregate(sent, fact=5)
+senta5 = aggregate (sent, fact=5)
 ncell(senta5) * nlyr(senta5)
-# 101760
+#101760
 
-# Exercise: make a multiframe and plot in RGB the three images (or, 2, 5)
+#Exercise:  make a multiframe and plot in RGB the three images (original, factor 2 and factor 5)
 im.multiframe(1,3)
-im.plotRGB(sent, 1, 2, 3)
-im.plotRGB(senta, 1, 2, 3)
-im.plotRGB(senta5, 1, 2, 3)
+im.plotRGB(sent, r=1, g=2, b=3)
+im.plotRGB(senta, r=1, g=2, b=3)
+im.plotRGB(senta5, r=1, g=2, b=3)
 
-# Calculating standard deviation
+#Calculating standard deviation
 nira = senta[[1]]
-sd3a = focal(nira, w=c(3,3), fun="sd")
+sda3a = focal(nira, w=c(3,3), fun="sd")
 
-# Exercise: calculate the standard deviation for the factor 5 image
+#Exercise: calculate the standard deviation for the factor 5 image
 nira5 = senta5[[1]]
 sd3a5 = focal(nira5, w=c(3,3), fun="sd")
 
-sd5a5 = focal(nira5, w=c(5,5), fun="sd")
+sda5a5 = focal(nira5, w=c(3,3), fun="sd")
 plot(sd5a5)
 
-im.multiframe(1,2)
+im.multiframe(2,2)
 plot(sd5a5)
 plot(sd3a)
+
+sent = im.import("sentinel.png")
+sent= flip(sent)
+nir= sent[[1]]
+plot(nir)
+
+sd3 = focal(nir, w=c(3,3), fun=sd)
+plot(sd3)
+
 
 im.multiframe(2,2)
 plot(sd3)
@@ -111,22 +125,15 @@ plot(sd3a)
 plot(sd3a5)
 plot(sd5a5)
 
-p1 = im.ggplot(sd3)
-p2 = im.ggplot(sd3a)
-p3 = im.ggplot(sd3a5)
-p4 = im.ggplot(sd5a5)
+p1 = im.ggplot(sd3, col=plasma(100))
+p2 = im.ggplot(sd3a, col=plasma(100))
+p3 = im.ggplot(sd3a5, col=plasma(100))
+p4 = im.ggplot(sd5a5,col=plasma(100))
 
-p1 + p2 + p3 + p4
+p1 + p2 p3 + p4
 
-
-im.multiframe(2,2)
-plot(sd3, col=mako(100))
-plot(sd3a, col=mako(100))
-plot(sd3a5, col=mako(100))
-plot(sd5a5, col=mako(100))
-
-# Variance
-# nir 
+#Variance
+#nir
 var3 = sd3^2
 
 dev.off()
@@ -140,4 +147,3 @@ sd5 = focal(nir, w=c(5,5), fun="sd")
 var5 = sd5^2
 plot(sd5)
 plot(var5)
-
