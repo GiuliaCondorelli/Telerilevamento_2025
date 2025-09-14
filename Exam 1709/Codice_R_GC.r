8a24ebbfd67f22f19fd9adb2055d5c487ffd22d5
GiuliaCondorelli <giuliacondorelli99@gmail.com>
2025-09-14 17:20:07 +0200

    Update Codice_R_GC.r

--- Exam 1709/Codice_R_GC.r
+++ Exam 1709/Codice_R_GC.r
@@ -1,12 +1,12 @@
-# PROGETTO D'ESAME (17/09/2025) - ANALISI DELL'IMPATTO DEI PASCOLI SULLA VEGETAZIONE DEL CAMPO IMPERATORE DAL 2015 AL 2024
+# PROGETTO D'ESAME (17/09/2025) - ANALISI DELL'IMPATTO DEI PASCOLI SULLA VEGETAZIONE DEL CAMPO IMPERATORE DAL 2015 AL 2025
 # Telerilevamento geologico in R
 # Giulia Condorelli
 
 # CODICE IN R PER L'ELABORAZIONE DELLE IMMAGINI
 
-# Le immagini vogliono mettere in evidenza i cambiamenti nella vegetazione negli ultimi anni a causa di pascoli poco regolamentati, aumentata siccità e elevato turismo
+# Le immagini vogliono mettere in evidenza i cambiamenti nella vegetazione nell'ultimo decennio a causa di pascoli poco regolamentati, aumentata siccità e elevato turismo
 # Grazie a Google Earth Engine sono state scaricate due immagini di Sentinel-2 (per il codice si veda lo script Esame.js)
-# La prima riguarda una mediana delle immagini dell'estate 2015, la seconda una mediana delle immagini dell'estate 2024
+# La prima riguarda una mediana delle immagini dell'estate 2015, la seconda una mediana delle immagini dell'estate 2025
 
 # Il salvataggio delle immagini da R è stato fatto con il menù a tendina di R, in formato .png
 
