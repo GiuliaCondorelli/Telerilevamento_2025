8ac6a32e8145d7ec716c7c7359e518402fbbddfe
GiuliaCondorelli <giuliacondorelli99@gmail.com>
2025-09-14 21:58:40 +0200

    Update Codice_R_GC.r

--- Exam 1709/Codice_R_GC.r
+++ Exam 1709/Codice_R_GC.r
@@ -1,5 +1,5 @@
 # PROGETTO D'ESAME (17/09/2025) - ANALISI DELL'IMPATTO DEI PASCOLI SULLA VEGETAZIONE DEL CAMPO IMPERATORE DAL 2015 AL 2025
-# Telerilevamento geologico in R
+# Telerilevamento geo-ecologico in R
 # Giulia Condorelli
 
 # CODICE IN R PER L'ELABORAZIONE DELLE IMMAGINI
@@ -22,3 +22,19 @@ setwd("C://Users/giuli/OneDrive/telexam/")
 # ---
 # IMPORTAZIONE DELLE IMMAGINI
 # Importo in R le immagini scaricate con Google Earth Engine
+campoimp15 <- rast("CampoImp2015.tif")
+campoimp15           # Per visualizzare le specifiche del raster
+plot(campoimp15)     # Plot che permette di visualizzare l'immagine
+dev.off()            # Chiudo il pannello grafico
+
+campoimp25 <- rast("CampoImp2025.tif")
+campoimp25
+plot(campoimp25)
+dev.off()
+
+# Visualizzo entrambe le immagini in RGB creando un multiframe
+im.multiframe(1,2)
+plotRGB(campoimp15, r = 1, g = 2, b = 3, stretch = "lin", main = "Campo Imperatore, 2015")
+plotRGB(campoimp25, r = 1, g = 2, b = 3, stretch = "lin", main = "Campo Imperatore, 2025")
+dev.off()           # Chiudo il pannello grafico dopo aver salvato l'immagine in .png
+
