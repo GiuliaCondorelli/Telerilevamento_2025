8d16e9d69c29f2bfde3a2bc01eb587d6206c780a
GiuliaCondorelli <giuliacondorelli99@gmail.com>
2025-09-16 23:42:09 +0200

    Update Code.R

--- Exam 1709/Code.R
+++ Exam 1709/Code.R
@@ -53,10 +53,10 @@ plot(campoimp25[[3]], main = "B2 - Blue", col = magma(100))
 plot(campoimp25[[4]], main = "B8 - NIR", col = magma(100))
 dev.off()          # Chiudo il pannello grafico dopo aver salvato l'immagine in .png
 
-# Le immagini col NIR (Near-InfraRed) sono molto importanti perchè sono quelle che ci permettono di osservare lo stato di salute della vegetazione. 
+# Le immagini col NIR (Near-InfraRed) sono molto importanti perché sono quelle che ci permettono di osservare lo stato di salute della vegetazione. 
 # Una minor riflessione del NIR mi indica una vegetazione sottoposta a stress. 
 
-# Dunque sostituendo la banda del blu al NIR posso evidenziare tutto ciò che è vegetazione in blu mentre tutto ciò che non lo è in giallo.
+# Dunque, sostituendo la banda del blu al NIR posso evidenziare tutto ciò che è vegetazione in blu mentre tutto ciò che non lo è in giallo.
 im.multiframe(1,2)
 plotRGB(campoimp15, r = 1, g = 2, b = 4, stretch="lin", main = "Campo Imperatore, 2015")   # Imposto il NIR sulla banda Blu
 plotRGB(campoimp25, r = 1, g = 2, b = 4, stretch="lin", main = "Campo Imperatore, 2025")
